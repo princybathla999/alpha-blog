0e6e8ef1943b052ad707a72208d651617ccd15d8
Alpha Blog <princybathla999@yahoo.com>
2017-04-20 20:04:26 -0400

    showing articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,36 @@
 class ArticlesController < ApplicationController
 
-def new
-@article =Article.new
-end
- def create
-   @article = Article.new(article_params)
-   @article.save
-   end
+  def new
+    @article =Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+    flash[:notice]= "Article was successfully created"
+    redirect_to article_path(@article)
+    else
+    render 'new'
+    end
+  end
+
+  def show
+  @article = Article.find(params[:id] )
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
   private
   def article_params
-    params.require(:article).permit(:title,:description)
+    params.require(:article).permit(:title, :description)
   end
-  end
+end
