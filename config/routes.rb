bbc5923b95b1a14ddef163cfc81af5328dff3c00
Alpha Blog <princybathla999@yahoo.com>
2017-04-22 16:43:01 -0400

    sign up user

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root 'pages#homepage'
   get 'about', to: 'pages#about'
   resources :articles
+  get 'signup', to: "users#new"
+  resources :users, except: [:new]
 end
