3dda7dacc299378e7e48cd414aac2553d8438c8b
Alpha Blog <princybathla999@yahoo.com>
2017-04-22 17:02:39 -0400

    edit user functionality and partials

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user,only: [:edit, :update, :show, :destroy  ]
   def index
   end
   def new
@@ -6,25 +7,37 @@ class UsersController < ApplicationController
   end
 
   def create
-
     @user = User.new(user_params)
-
     if @user.save
-
       flash[:success] = "Welcome to the alpha blog #{@user.username}"
-
       redirect_to articles_path
-
     else
-
       render 'new'
-
     end
 
   end
+  def edit
+  end
 
+  def update
+   if @user.update_attributes(user_params)
+   flash[:success]= "User Updated Successfully"
+    redirect_to articles_path
+   else
+     render 'edit'
+   end
+  end
+
+  def show
+
+  end
+  def destroy
+
+  end
   private
-
+  def set_user
+  @user = User.find(params[:id] )
+  end
   def user_params
 
     params.require(:user).permit(:username, :email, :password)
